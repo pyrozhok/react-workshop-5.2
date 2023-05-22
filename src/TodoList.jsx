23a686327c232617377d21d1cc1661fbd2583bca
Alexander Sopin <alex.sopin0803@gmail.com>
2023-05-22 21:20:37 +0500

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,19 @@
+import './todo-list.css';
+function TodoList(props) {
+  const { data, onDelete } = props;
+
+  const todoList = data.map((todo) => <li key={todo.id}>{todo.title}</li>);
+
+  return (
+    <div className="todo-list">
+      <h2>Задачи</h2>
+      {todoList.length > 0 ? (
+        <ul className="todo-list__items">{todoList}</ul>
+      ) : (
+        <div>Пока нет задач</div>
+      )}
+    </div>
+  );
+}
+
+export default TodoList;
